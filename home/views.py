244079202d70ffc413653b09a052a896b3320e54
Saurav <sp.gharti@gmail.com>
2018-04-13 00:45:19 +0000

    add package django-allauth

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
 	{'name':'satchless', 'url': 'http://pypi.python.org/pypi/satchless/1.1.3'},
 	{'name':'django-shop', 'url': 'http://pypi.python.org/pypi/django-shop/0.11.3'},
     ]
